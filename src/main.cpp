59f22e7042a81be51c3f8b6918efbc4f322f094f
TomasLeTrain <tomylopezd@gmail.com>
2025-10-29 11:01:53 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <iostream>
+
+void initialize() {}
+
+void disabled() {}
+
+void competition_initialize() {}
+
+void autonomous() {}
+
+void opcontrol() {
+	std::cout << "hello world!" << std::endl;
+}
